8d5042d42a8a78292d5dcad586eb36457e5c940d
Gabriel Poça <gabrielpoca@gmail.com>
2012-11-24 17:01:12 +0000

    client server and agent basics done

--- StoneAgeServer/src/stoneageserver/StoneAgeServer.java
+++ StoneAgeServer/src/stoneageserver/StoneAgeServer.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package stoneageserver;
 
-/**
- *
- * @author gabriel
- */
 public class StoneAgeServer {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here/
     }
 }
