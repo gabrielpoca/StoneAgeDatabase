11082547cbc46d01de5b3ffaaa652c450c8026d6
Gabriel Poça <gabrielpoca@gmail.com>
2012-11-26 09:47:07 +0000

    sync server started

--- StoneAgeServer/src/syncserver/SyncServer.java
+++ StoneAgeServer/src/syncserver/SyncServer.java
@@ -1,11 +1,9 @@
 package syncserver;
 
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
 public class SyncServer extends Thread {
